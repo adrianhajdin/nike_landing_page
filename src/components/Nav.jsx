6110e1797b63967b3b0f1ec0f966001cc61d3ca4
Geoffrey <gmwangi3174@gmail.com>
2023-08-22 12:48:58 +0300

    done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,36 +4,36 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <a href='/'>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
           <img
             src={headerLogo}
-            alt='logo'
+            alt="logo"
             width={129}
             height={29}
-            className='m-0 w-[129px] h-[29px]'
+            className="m-0 w-[129px] h-[29px]"
           />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className="font-montserrat leading-normal text-lg text-slate-gray hover:bg-coral-red hover:text-white hover:rounded-xl hover:p-3"
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
-          <a href='/'>Sign in</a>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
           <span>/</span>
-          <a href='/'>Explore now</a>
+          <a href="/">Explore now</a>
         </div>
-        <div className='hidden max-lg:block'>
-          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
         </div>
       </nav>
     </header>
